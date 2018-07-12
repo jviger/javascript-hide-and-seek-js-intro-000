2bc0b401c650007715eb544b615515e3f2119883
Jason Viger <jason.viger@gmail.com>
2018-07-12 05:36:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nestedTarget(){
 function increaseRankBy(n){
  const lis = document.querySelectorAll('ul.ranked-list li');
  for (let i=0; i< lis.length; i++){
-   lis[i].innerHTML = i + n + 1;
+   lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n);
    console.log(lis[i].value);
  }
 
